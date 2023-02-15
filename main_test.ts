e50fe6bab3ecd6e0abebd146451e0f43f0591cfe
Masahiro Kondou <kondoumh@gmail.com>
2023-02-15 10:44:52 +0900

    add subtract (#2)

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,10 @@
 import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
-import { add } from "./main.ts";
+import { add, subtract } from "./main.ts";
 
 Deno.test(function addTest() {
   assertEquals(add(2, 3), 5);
+});
+
+Deno.test(function subtractTest() {
+  assertEquals(subtract(2, 3), -1);
 });
